25ec35867608b7ce08df65051ac369b62ce92d85
a.danilov <a.danilov@upstars.com>
2026-05-12 14:47:31 +0300

    fix: UN-2673 fix config for connecting

--- src/tailwind/presets/layout.preset.ts
+++ src/tailwind/presets/layout.preset.ts
@@ -84,10 +84,6 @@ const preset: Partial<Config> = {
             width: {
                 '84': '21rem',
             },
-            maxWidth: {
-                layout: "var(--main-layout-width)",
-                'full-mob': "calc(100% - 2rem)",
-            },
             opacity: {
                 '80': '.80',
             }
